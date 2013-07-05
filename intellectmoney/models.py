3ade0ba8607498b4e23b9d80450332f8d0508d93
Victor Safronovich <vsafronovich@gmail.com>
2013-07-05 16:28:43 +0600

    remove generic object, use orderId for complex id of the object

--- intellectmoney/models.py
+++ intellectmoney/models.py
@@ -3,20 +3,11 @@ import datetime
 
 from django.db import models
 
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
-
-
 class IntellectMoney(models.Model):
 
     created = models.DateTimeField(editable=False,
                                    default=datetime.datetime.now)
     orderId = models.CharField(unique=True, editable=False, max_length=255)
 
-    content_type = models.ForeignKey(ContentType,
-                                     blank=True, null=True)
-    object_id = models.PositiveIntegerField(blank=True, null=True)
-    recipient = generic.GenericForeignKey('content_type', 'object_id')
-
     def __unicode__(self):
-        return u'IM payment %s for %s' % (self.orderId, self.recipient)
+        return u'IM payment for %s' % (self.orderId,)
