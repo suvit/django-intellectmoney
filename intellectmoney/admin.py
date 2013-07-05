cbab36bc70fbefd23e649de5a4c9792bb1d874ec
Victor Safronovich <vsafronovich@gmail.com>
2013-07-05 16:09:52 +0600

    added recipient to im model

---
+++ intellectmoney/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from intellectmoney.models import IntellectMoney
+
+admin.site.register(IntellectMoney)
--- intellectmoney/models.py
+++ intellectmoney/models.py
@@ -3,6 +3,9 @@ import datetime
 
 from django.db import models
 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
 
 class IntellectMoney(models.Model):
 
@@ -10,3 +13,10 @@ class IntellectMoney(models.Model):
                                    default=datetime.datetime.now)
     orderId = models.CharField(unique=True, editable=False, max_length=255)
 
+    content_type = models.ForeignKey(ContentType,
+                                     blank=True, null=True)
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+    recipient = generic.GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return u'IM payment %s for %s' % (self.orderId, self.recipient)
